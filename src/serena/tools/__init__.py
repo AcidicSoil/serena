aef7c31eca2762992710e9ac7120aff0c53224ab
DirtyData <51036884+AcidicSoil@users.noreply.github.com>
2026-08-07 20:48:01 -0500

    Add external Serena tool package discovery (#1)
    * add external Serena tool package discovery
    
    * load external Serena tool packages
    
    * test external Serena tool package discovery
    
    * format external tool loader

--- src/serena/tools/__init__.py
+++ src/serena/tools/__init__.py
@@ -8,3 +8,6 @@ from .config_tools import *
 from .workflow_tools import *
 from .jetbrains_tools import *
 from .query_project_tools import *
+from .external_tool_loader import load_external_tool_packages
+
+load_external_tool_packages()
